0f111b6cca04acc7f83ae474ae0baaba9420fa36
Aman Chauhan <125898809+amanchauhan786@users.noreply.github.com>
2025-03-24 03:42:44 +0530

    Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from 'lucide-react';
 
 // Sample profile images (you would replace these with actual images)
 const profileImages = [
-  'amanchauhan.JPG',
+  'https://github.com/amanchauhan786/soothing-visuals/blob/e1b074e033aca2f0ba90d3072f9bade3da7f0952/amanchauhan.JPG',
   'https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2670&auto=format&fit=crop',
   'https://images.unsplash.com/photo-1488229297570-58520851e868?q=80&w=2669&auto=format&fit=crop'
 ];
