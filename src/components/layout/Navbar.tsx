067b4967101ee5a061e9f609509fe9156350b1f7
Aman Chauhan <125898809+amanchauhan786@users.noreply.github.com>
2025-03-25 15:52:33 +0530

    Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -49,7 +49,7 @@ export const Navbar: React.FC = () => {
           href="#home" 
           className="text-xl font-bold tracking-tight relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-primary after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300"
         >
-          Aman Chauhan
+          AC
         </a>
 
         {/* Desktop navigation */}
